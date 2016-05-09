eff49a1ea29dbc78a17324ea37b81a2a27c6e8c4
Shanavas Veedu <shanavaskv@gmail.com>
2016-05-08 21:20:55 -0400

    empty

--- public/app/alerts.js
+++ public/app/alerts.js
@@ -1,37 +1,37 @@
-app.controller('AlertsCtrl', function($scope, Product, ngProgress, toaster) {
+app.controller('AlertsCtrl', function($scope, Alert, ngProgress, toaster) {
 
-$scope.alert = new Alert();
+  $scope.alert = new Alert();
 
-var refresh = function() {
-  $scope.alerts = Alert.query();
-  $scope.alert = "";
-}
-refresh();
+  var refresh = function() {
+    $scope.alerts = Alert.query();
+    $scope.alert = "";
+  }
+  refresh();
 
-$scope.add = function(alert) {
-  Alert.save(alert,function(alert){
-    refresh();
-  });
-};
+  $scope.add = function(alert) {
+    Alert.save(alert,function(alert){
+      refresh();
+    });
+  };
 
-$scope.update = function(alert) {
-  alert.$update(function(){
-    refresh();
-  });
-};
+  $scope.update = function(alert) {
+    alert.$update(function(){
+      refresh();
+    });
+  };
 
-$scope.remove = function(alert) {
-  alert.$delete(function(){
-    refresh();
-  });
-};
+  $scope.remove = function(alert) {
+    alert.$delete(function(){
+      refresh();
+    });
+  };
 
-$scope.edit = function(id) {
-  $scope.alert = Alert.get({ id: id });
-};
+  $scope.edit = function(id) {
+    $scope.alert = Alert.get({ id: id });
+  };
 
-$scope.deselect = function() {
-  $scope.alert = "";
-}
+  $scope.deselect = function() {
+    $scope.alert = "";
+  }
 
 })
